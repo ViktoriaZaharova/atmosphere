06a1ec9a5156b9e7ef4e2acd9cf149af4b1119e9
Виктория Захарова <zaharova181114@gmail.com>
2023-10-29 00:22:43 +0300

    add pages

--- js/common.js
+++ js/common.js
@@ -51,3 +51,30 @@ $(function () {
 	});
 });
 //end
+
+Fancybox.bind("[data-fancybox]", {
+	// Your custom options
+});
+
+// slick slider
+$('.employee-slider').slick({
+	slidesToShow: 3,
+	dots: true,
+	prevArrow: '<button type="button" class="slick-prev"></button>',
+	nextArrow: '<button type="button" class="slick-next"></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+			}
+		}
+	]
+});
